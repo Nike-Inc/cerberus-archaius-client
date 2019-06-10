00dbab7b1f3558f2912f7bb0ba434e1968f06b57
davidzhao98 <david_zhao98@msn.com>
2019-06-10 10:39:52 -0700

    chore: upgrade java client to 7.0.1

--- src/main/java/com/nike/cerberus/archaius/client/ArchaiusCerberusClientFactory.java
+++ src/main/java/com/nike/cerberus/archaius/client/ArchaiusCerberusClientFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Nike, Inc.
+ * Copyright (c) 2019 Nike, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,36 +16,90 @@
 
 package com.nike.cerberus.archaius.client;
 
+import com.amazonaws.regions.Regions;
+import com.netflix.config.ConfigurationManager;
 import com.nike.cerberus.client.auth.DefaultCerberusCredentialsProviderChain;
 import com.nike.cerberus.client.CerberusClient;
 import com.nike.cerberus.client.CerberusClientFactory;
+import okhttp3.HttpUrl;
+import org.apache.commons.configuration.AbstractConfiguration;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Client factory for creating a Cerberus client with URL resolved using the ArchaiusCerberusUrlResolver
+ * Client factory for creating a Cerberus client
  */
 public class ArchaiusCerberusClientFactory {
 
+    private static final String CERBERUS_ADDR_ENV_PROPERTY = "CERBERUS_ADDR";
+
+    private static final String CERBERUS_ADDR_SYS_PROPERTY = "cerberus.addr";
+
+    private static final String CERBERUS_REGION_ENV_PROPERTY = "CERBERUS_REGION";
+
+    private static final String CERBERUS_REGION_SYS_PROPERTY = "cerberus.region";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArchaiusCerberusClientFactory.class);
+
     /**
-     * Resolves the Cerberus/Cerberus URL via the {@link ArchaiusCerberusUrlResolver} and creates a new {@link CerberusClient}
-     * with the {@link DefaultCerberusCredentialsProviderChain}.
+     * Creates a new {@link CerberusClient} with the {@link DefaultCerberusCredentialsProviderChain}.
      *
      * @return Cerberus client
      */
     public static CerberusClient getClient() {
-        final ArchaiusCerberusUrlResolver archaiusUrlResolver = new ArchaiusCerberusUrlResolver();
-
         final Map<String, String> defaultHeaders = new HashMap<>();
         final String xCerberusClientHeaderValue = ClientVersion.getClientHeaderValue();
         defaultHeaders.put(ClientVersion.CERBERUS_CLIENT_HEADER, xCerberusClientHeaderValue);
 
         return CerberusClientFactory.getClient(
-                archaiusUrlResolver,
+                resolveUrl(),
                 // pass the client HTTP header value to be used in authenticate calls to Cerberus
-                new DefaultCerberusCredentialsProviderChain(archaiusUrlResolver, xCerberusClientHeaderValue),
+                new DefaultCerberusCredentialsProviderChain(resolveUrl(), getRegion(), xCerberusClientHeaderValue),
                 // pass the client header to be used in all other calls to Cerberus (e.g. read, write, etc.)
                 defaultHeaders);
+    }
+
+    /**
+     * Attempts to acquire Cerberus URL from Archaius.
+     *
+     * @return Cerberus URL
+     */
+    private static String resolveUrl() {
+        final AbstractConfiguration configuration = ConfigurationManager.getConfigInstance();
+        final String envUrl = configuration.getString(CERBERUS_ADDR_ENV_PROPERTY);
+        final String sysUrl = configuration.getString(CERBERUS_ADDR_SYS_PROPERTY);
+
+        if (StringUtils.isNotBlank(envUrl) && HttpUrl.parse(envUrl) != null) {
+            return envUrl;
+        } else if (StringUtils.isNotBlank(sysUrl) && HttpUrl.parse(sysUrl) != null) {
+            return sysUrl;
+        } else {
+            LOGGER.warn("Unable to resolve the Cerberus URL.");
+            throw new IllegalArgumentException("Cerberus Address property is not configured correctly.");
+        }
+    }
+
+    /**
+     * Attempts to acquire Cerberus region from Archaius.
+     *
+     * @return Cerberus region
+     */
+    private static String getRegion() {
+        final AbstractConfiguration configuration = ConfigurationManager.getConfigInstance();
+        final String envRegion = configuration.getString(CERBERUS_REGION_ENV_PROPERTY);
+        final String sysRegion = configuration.getString(CERBERUS_REGION_SYS_PROPERTY);
+
+        if (StringUtils.isNotBlank(envRegion) && Regions.fromName(envRegion) != null) {
+            return envRegion;
+        } else if (StringUtils.isNotBlank(sysRegion) && Regions.fromName(sysRegion) != null) {
+            return sysRegion;
+        } else {
+            LOGGER.warn("Unable to get the Cerberus region.");
+            throw new IllegalArgumentException("Cerberus Region property is not configured correctly.");
+        }
     }
 }
